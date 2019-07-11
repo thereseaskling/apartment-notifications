33163e63e2d617130b6fd587c1efe540601d05e8
Therese Askling <therese.askling@gmail.com>
2019-07-11 21:38:19 +0200

    fixed the volume for the docker container

--- start_worker.sh
+++ start_worker.sh
@@ -2,5 +2,5 @@ mvn clean package
 docker stop apartments
 docker rm apartments
 docker build -t apartments .
-docker run -t -p 8443:8443 --name apartments apartments
+docker run -t -p 8443:8443 --volume ~/Repos/apartment-notifications/saved_information:/saved_information --name apartments apartments
 
